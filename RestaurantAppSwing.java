418d4ea47680e3ee7389ce1a579290a7fe8a69a8
Aida <153488331+zhxauda9@users.noreply.github.com>
2024-11-06 16:28:46 +0500

    updated RestaurantAppSwing.java

--- RestaurantAppSwing.java
+++ RestaurantAppSwing.java
@@ -1,11 +1,17 @@
 package FinalProject;
 
+import FinalProject.Internal.Factory.*;
+import FinalProject.Internal.Objects.Dish;
+import FinalProject.Internal.Objects.DishCategory;
+import FinalProject.Internal.Objects.Order;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class RestaurantAppSwing {
     private JFrame frame;
@@ -13,25 +19,48 @@ public class RestaurantAppSwing {
     private JTextArea orderTextArea;
     private List<Dish> menu;
     private Order currentOrder;
+    private JComboBox<String> categoryComboBox;
 
     public RestaurantAppSwing() {
+        currentOrder = Order.getInstance();
         menu = new ArrayList<>();
-        currentOrder = new Order();
         initializeMenu();
         initializeUI();
     }
 
     private void initializeMenu() {
-        menu.add(new Dish("Паста", 1200));
-        menu.add(new Dish("Пицца", 1500));
-        menu.add(new Dish("Салат", 800));
+        DishFactory mainDishFactory = new MainDishFactory();
+        DishFactory drinkFactory = new DrinkFactory();
+        DishFactory snackFactory = new SnackFactory();
+        DishFactory dessertFactory = new DessertFactory();
+
+        menu.add(mainDishFactory.createDish("Паста", 1200));
+        menu.add(mainDishFactory.createDish("Пицца", 1500));
+        menu.add(snackFactory.createDish("Салат", 800));
+        menu.add(drinkFactory.createDish("Кофе", 600));
+        menu.add(drinkFactory.createDish("Чай", 500));
+        menu.add(dessertFactory.createDish("Торт", 1200));
+        menu.add(dessertFactory.createDish("Мороженое", 700));
     }
 
     private void initializeUI() {
         frame = new JFrame("Онлайн-заказ ресторана");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(500, 400);
         frame.setLayout(new FlowLayout());
+
+        // Category filter
+        JLabel categoryLabel = new JLabel("Выберите категорию:");
+        frame.add(categoryLabel);
+
+        categoryComboBox = new JComboBox<>(new String[] {"Все", "Основные блюда", "Напитки", "Закуски", "Десерты"});
+        categoryComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateDishComboBox();
+            }
+        });
+        frame.add(categoryComboBox);
 
         JLabel menuLabel = new JLabel("Выберите блюдо:");
         frame.add(menuLabel);
@@ -64,7 +93,34 @@ public class RestaurantAppSwing {
         });
         frame.add(finalizeButton);
 
+        JButton clearButton = new JButton("Удалить все заказы");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentOrder.clearOrder();
+                updateOrderTextArea();
+            }
+        });
+        frame.add(clearButton);
+
         frame.setVisible(true);
+    }
+
+    private void updateDishComboBox() {
+        String selectedCategory = (String) categoryComboBox.getSelectedItem();
+        List<Dish> filteredDishes = menu;
+
+        if (selectedCategory.equals("Основные блюда")) {
+            filteredDishes = menu.stream().filter(dish -> dish.getCategory() == DishCategory.MAIN_COURSE).collect(Collectors.toList());
+        } else if (selectedCategory.equals("Напитки")) {
+            filteredDishes = menu.stream().filter(dish -> dish.getCategory() == DishCategory.DRINK).collect(Collectors.toList());
+        } else if (selectedCategory.equals("Закуски")) {
+            filteredDishes = menu.stream().filter(dish -> dish.getCategory() == DishCategory.SNACK).collect(Collectors.toList());
+        } else if (selectedCategory.equals("Десерты")) {
+            filteredDishes = menu.stream().filter(dish -> dish.getCategory() == DishCategory.DESSERT).collect(Collectors.toList());
+        }
+
+        dishComboBox.setModel(new DefaultComboBoxModel<>(filteredDishes.toArray(new Dish[0])));
     }
 
     private void updateOrderTextArea() {
@@ -79,47 +135,3 @@ public class RestaurantAppSwing {
         SwingUtilities.invokeLater(RestaurantAppSwing::new);
     }
 }
-
-class Dish {
-    private String name;
-    private double price;
-
-    public Dish(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    @Override
-    public String toString() {
-        return name + " - " + price + " тг";
-    }
-}
-
-class Order {
-    private List<Dish> dishes;
-
-    public Order() {
-        dishes = new ArrayList<>();
-    }
-
-    public void addDish(Dish dish) {
-        dishes.add(dish);
-    }
-
-    public List<Dish> getDishes() {
-        return dishes;
-    }
-
-    public double calculateTotal() {
-        return dishes.stream().mapToDouble(Dish::getPrice).sum();
-    }
-}
-
