a9b1673a2d9b4bbaa2d38a8c9e22a06e3685cc76
meruyert4 <147834105+meruyert4@users.noreply.github.com>
2024-11-06 17:05:02 +0500

    added logic for observer in order

--- Internal/Objects/Order.java
+++ Internal/Objects/Order.java
@@ -1,14 +1,18 @@
+// File: FinalProject/Internal/Objects/Order.java
 package FinalProject.Internal.Objects;
 
+import FinalProject.Internal.Observers.OrderObserver;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
     private static Order instance;
     private List<Dish> dishes;
+    private List<OrderObserver> observers;
 
     private Order() {
         dishes = new ArrayList<>();
+        observers = new ArrayList<>();
     }
 
     public static Order getInstance() {
@@ -18,12 +22,33 @@ public class Order {
         return instance;
     }
 
+    public void addObserver(OrderObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(OrderObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (OrderObserver observer : observers) {
+            observer.updateOrder();
+        }
+    }
+
     public void addDish(Dish dish) {
         dishes.add(dish);
+        notifyObservers();
+    }
+
+    public void removeDish(Dish dish) {
+        dishes.remove(dish);
+        notifyObservers();
     }
 
     public void clearOrder() {
         dishes.clear();
+        notifyObservers();
     }
 
     public List<Dish> getDishes() {
@@ -32,11 +57,5 @@ public class Order {
 
     public double calculateTotal() {
         return dishes.stream().mapToDouble(Dish::getPrice).sum();
-    }
-
-    public void removeDish(Dish dish) {
-        if (dishes.contains(dish)) {
-            dishes.remove(dish);
-        }
     }
 }
